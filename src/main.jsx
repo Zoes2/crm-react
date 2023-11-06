92f8a150a5981e34745315b09c02e595f593aa23
Zoes2 <v.meneses20.20@gmail.com>
2023-11-06 17:28:35 -0300

    agregamos función editar y correción de errores

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import NuevoCliente, {action as nuevoClienteAction} from './pages/NuevoCliente'
 import Index, { loader as clientesLoader } from './pages/Index'
+import EditarCliente, { loader as editarClienteLoader,action as editarClienteAction } from './pages/EditarCliente'
 import ErrorPage from './components/ErrorPage'
 
 const router = createBrowserRouter([
@@ -22,7 +23,16 @@ const router = createBrowserRouter([
       {
         path: '/clientes/nuevo',
         element: <NuevoCliente />,
-        action: nuevoClienteAction
+        action: nuevoClienteAction,
+        errorElement: <ErrorPage />
+      },
+
+      {
+        path: '/clientes/:clienteId/editar',
+        element: <EditarCliente />,
+        loader: editarClienteLoader,
+        action: editarClienteAction,
+        errorElement: <ErrorPage />
       }
     ]
   },
